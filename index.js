ba4ef5a4c8336d4047d6f91c23d5b81e042a5df9
Savannah <savmaxwell@gmail.com>
2021-10-11 13:49:12 -0600

    completed task 1

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+      In counter1 count is defined in the functional scope while counter2 uses global scope to define count
   2. Which of the two uses a closure? How can you tell?
-  
+      counterMaker uses a closure at return count, because it's using a variable that was defined in its outer function.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?
+      counter2 would be more useful if you wanted to keep count consistent throughout your code, while counter1 would be useful if you wanted to count different things using one function ie. const counter1 = counterMaker() and const balloonCount = counterMaker() etc.
 */
 
 // counter1 code
@@ -45,6 +46,11 @@ function counterMaker() {
 
 const counter1 = counterMaker();
 
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+
+
 // counter2 code
 let count = 0;
 
@@ -52,6 +58,9 @@ function counter2() {
   return count++;
 }
 
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
 
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
